cf8af42922cb4c9c69b6e5bd4ba7ed04a7c0f781
Evren Kasap <evrkll9@gmail.com>
2021-11-18 22:38:11 +0300

    Add project files.

---
+++ Odev_18112021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Odev_18112021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
